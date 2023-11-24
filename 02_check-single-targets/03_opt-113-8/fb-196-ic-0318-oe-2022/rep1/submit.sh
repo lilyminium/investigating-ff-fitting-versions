8649691723afc1200af680d8236f31d22264060c
Lily Wang <lilywang@lilac-ln02.hpc.private>
2023-11-23 19:01:17 -0500

    add single target checks

---
+++ 02_check-single-targets/03_opt-113-8/fb-196-ic-0318-oe-2022/rep1/submit.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# Set the job name and wall time limit
+#BSUB -J master_01_fb-193
+#BSUB -W 168:00
+#
+# Set the output and error output paths.
+#BSUB -o  master_01_fb-193-%J.o
+#BSUB -e  master_01_fb-193-%J.e
+#
+# Set any cpu options.
+#BSUB -M 2
+
+# ===================== conda environment =====================
+
+source ~/.bashrc
+micromamba activate fb-196-ic-0318-oe-2022
+
+micromamba env export > fitting-environment.yaml
+
+export OE_LICENSE="/home/lilywang/oe_license.txt"
+
+mkdir working-directory
+
+ForceBalance.py optimize.in > force_balance.log
